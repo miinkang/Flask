d2f881c660f5f4e3562ffea0bb25b4414f90b367
miinkang <68461606+miinkang@users.noreply.github.com>
2020-11-28 15:34:34 +0900

    debug label

--- Flask_Examples/main.py
+++ Flask_Examples/main.py
@@ -33,10 +33,10 @@ def make_prediction():
         # 업로드 파일 처리 분기
 
         image_url = request.values['image']
-
-
+        if not 'http' in image_url: return render_template('index.html', label="No Image url")
 
         response = requests.get(image_url)
+        if not response: return render_template('index.html', label="No Image url")
         image = Image.open(BytesIO(response.content)).convert('RGB')
 
         # image = Image.open(image_url).convert('RGB')
@@ -44,18 +44,9 @@ def make_prediction():
         image = np.array(image)
         image = image.reshape((200, 200, 3))
 
-        # file = request.files['image']
-        # if not file: return render_template('index.html', label="No Files")
-        #
-        # # 이미지 픽셀 정보 읽기
-        # # 알파 채널 값 제거 후 1차원 Reshape
-        # img = misc.imread(file)
-        # img = img[:, :, :3]
-        # img = img.reshape(200,200,3)
-        # img = img.reshape(1, -1)
+
 
         # 입력 받은 이미지 예측
-        is_korea_flag = True
 
         # os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
@@ -65,21 +56,23 @@ def make_prediction():
         x_test = np.array(x_test)
         x_test = x_test / 255
 
+        is_korea_flag = True
+
         pred = model.predict(x_test)
         if pred < 0.5:
-            is_korea_flag = True
+            is_korea_flag = "태극기 입니다."
         else:
-            is_korea_flag = False
+            is_korea_flag = "태극기가 아닙니다."
 
 
         # 결과 리턴
-        return render_template('index.html', label=is_korea_flag)
+        return render_template('index.html', label=is_korea_flag, image_url=image_url)
 
 
 if __name__ == '__main__':
     # 모델 로드
     # ml/model.py 선 실행 후 생성
-    model = tf.keras.models.load_model('./model/xception.h5')
+    model = tf.keras.models.load_model('./model/inceptionv3_small_better.h5')
     # model.load_weights('./model/xception.h5')
     # Flask 서비스 스타트
     app.run(host='127.0.0.1', port=8000, debug=True)
